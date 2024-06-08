53ca60f0644e40a8fb9abd5a2c65f5f4d92eb629
giadyensukwabi <thurgid@gmail.com>
2024-06-08 05:04:38 +0000

    Made failed tracking user-friendly

--- app/track/[trip_id]/page.tsx
+++ app/track/[trip_id]/page.tsx
@@ -46,7 +46,7 @@ export default function TrackDelivery() {
     return (
       <main className="flex min-h-screen flex-col items-center gap-10 mb-20">
         <TopSectionContainer className="flex flex-col items-start justify-center gap-2 w-full h-80 bg-slate-800  p-4 text-white ">
-          <span className="text-5xl font-bold ">No data found</span>
+          <span className="text-5xl font-bold ">No data found for {trip_id}</span>
           <p className="text-lg font-bold">
             Check the trip ID and ensure that you are not missing a value and
             search again
