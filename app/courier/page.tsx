93863c218e3aa94e4d8185a5f45cba2d964e9468
giadyensukwabi <thurgid@gmail.com>
2024-06-10 04:40:04 +0000

    renamed 'dispatcher' page 'courier'

---
+++ app/courier/page.tsx
@@ -0,0 +1,8 @@
+import DispatcherDetail from "@/components/Dispatcher/DispatcherDetail"
+export default function Courer () {
+    return (
+        <main className="flex flex-col gap-3 p-4 items-center w-full justify-center mb-24 h-screen dark:bg-primary-dark">
+            <DispatcherDetail/>
+        </main>
+    )
+}
--- components/Dashboard/Nav/DashboardLoader.tsx
+++
@@ -1,8 +0,0 @@
-import { InfinityLoader } from "../../Shared/Loader"
-export default function DashboardLoader () {
-    return (
-        <main className="h-screen w-full flex items-center justify-center dark:bg-primary-dark ">
-            <InfinityLoader/>
-        </main>
-    )
-}
