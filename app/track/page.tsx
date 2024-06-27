93e6506da16c16b4b3c85e41fe3955a11ae7de4b
giadyensukwabi <thurgid@gmail.com>
2024-06-27 11:16:42 +0000

    mini UI change

--- app/track/page.tsx
+++ app/track/page.tsx
@@ -1,8 +1,15 @@
+'use client'
 import MiddleSectionContainer from "@/components/Shared/MiddleSectionContainer";
 import TopSectionContainer from "@/components/Shared/TopSectionContainer";
 
 import TrackOrder from "@/components/Order/TrackOrder/UserTrackOrder";
+import { useAppSelector } from "@/lib/store/hooks";
+import { redirect } from "next/navigation";
+
 export default function TrackDelivery() {
+  //redirect users who are not customers
+  const {user_role} = useAppSelector(state=>state.user);
+  user_role && user_role !== "customer" ? redirect('/profile') : ''
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 mb-20">
       <TopSectionContainer className="flex flex-col items-start justify-center gap-2 w-full h-80 bg-slate-800  p-4 text-white ">
