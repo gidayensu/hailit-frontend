5fe585fb7a1b179b2dfbeac741fdf9d0ca1f71f3
giadyensukwabi <thurgid@gmail.com>
2024-07-01 11:15:55 +0000

    Added transition to closing and opening sidebar

--- components/Dashboard/Nav/DashboardSections.tsx
+++ components/Dashboard/Nav/DashboardSections.tsx
@@ -15,16 +15,17 @@ export default function DashboardSections({
     return (
       <div
         onClick={() => onClickFunc(sectionName)}
-        className={`flex h-10 p-2  items-center w-full hover:text-primary-color hover:bg-white rounded-md text-[13px] ${
-          dashMin ? "justify-center" : "md:justify-between justify-center"
+        className={`flex h-8 p-2  items-center w-full hover:text-primary-color hover:bg-white rounded-md text-[12px] ${
+          dashMin ? "justify-center transition-all duration-300" : "md:justify-between justify-center transition-all duration-300"
         }  cursor-pointer ${
           activeSection === sectionName ? "bg-white text-primary-color" : ""
         }`}
       >
-        <div className="flex items-center justify-center gap-2 ">
+        <div className="flex items-center justify-center gap-2 transition-all duration-300 transform translate-x-[100%] md:translate-x-0">
           {children}
           {!dashMin && (
-            <p className="hidden md:inline text-md font-bold"> {sectionName} </p>
+            <p className="hidden md:inline text-md font-bold transition-all duration-300 transform translate-x-[100%] md:translate-x-0"> {sectionName} </p>
+
           )}
         </div>
       </div>
