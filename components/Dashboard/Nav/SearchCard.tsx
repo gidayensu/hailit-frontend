a711ff3962446dec8c87d3a459f13cce38757b36
giadyensukwabi <thurgid@gmail.com>
2024-07-01 11:17:30 +0000

    Fixed loader spinning while error has occurred

--- components/Dashboard/Nav/SearchCard.tsx
+++ components/Dashboard/Nav/SearchCard.tsx
@@ -16,10 +16,10 @@ interface SearchCardProps {
 
 
 const SearchCard = forwardRef<HTMLDivElement, SearchCardProps> (({trips, isLoading, error, handleSelectedTrip}, ref) => {         
-    
+    console.log({isLoading, error})
     return (
         <Container className="-ml-4 md:-ml-24 absolute w-72 md:w-[800px] text-[12px] max-h-80 z-10 mt-3 rounded-xl p-4 font-normal  flex flex-col items-center gap-2" ref= {ref}>
-          {isLoading && <Loader color="primary" /> } 
+          {isLoading && !error && <Loader color="primary" /> } 
           {
               error  &&
               <p>
