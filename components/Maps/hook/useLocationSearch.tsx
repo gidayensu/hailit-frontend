195bca273f62112225da8b74dc671ebea8640371
giadyensukwabi <thurgid@gmail.com>
2024-07-04 13:09:10 +0000

    Prettier Formatting

--- components/Maps/hook/useLocationSearch.tsx
+++ components/Maps/hook/useLocationSearch.tsx
@@ -1,57 +1,57 @@
-
 import {
-  setSearchCard,
-  setSearchData,setPickUpLocation,
   setDropOffLocation,
-  
+  setPickUpLocation,
+  setSearchCard,
+  setSearchData,
 } from "@/lib/store/slice/mapSlice";
 import { fetchMapData } from "@/lib/utils";
 
-import { useAppSelector, useAppDispatch } from "@/lib/store/hooks";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 
 import { UserLocation } from "../MainMap";
-import { getSpecificName } from "@/lib/utils";
 
-import SearchItem from "../SearchItem";
 import { LocationType } from "./useMap";
 
 import { useCallback, useRef, useState } from "react";
 
-export const useLocationSearch = (locationType?:LocationType)=> {
-
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+export const useLocationSearch = (locationType?: LocationType) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { searchData, searchContainer } = useAppSelector((state) => state.map);
   const dispatch = useAppDispatch();
-    const inputRef = useRef<any>(null);
+  const inputRef = useRef<any>(null);
 
-  const mapDataHandler = useCallback( async (e:any) => {
-    setIsLoading(true)
+  const mapDataHandler = useCallback(async (e: any) => {
+    setIsLoading(true);
     // const data: any = await fetchMapData(searchQuery?.target?.value);
-    const data: any = await  fetchMapData(e.target.value);
+    const data: any = await fetchMapData(e.target.value);
     if (data) {
-        
-        dispatch(setSearchData(data))
-        setIsLoading(false)
+      dispatch(setSearchData(data));
+      setIsLoading(false);
     }
-dispatch(setSearchCard(true));
-  }, []);
+    dispatch(setSearchCard(true)); //TODO: CHANGE FROM GLOBAL TO LOCAL STATE
+  }, [ setSearchCard, dispatch, setIsLoading, dispatch]);
 
-const { searchData,  searchContainer } = useAppSelector((state) => state.map);
-  
-    
-    const selectedSearchItemHandler = ({
+  const selectedSearchItemHandler = ({
     locationName,
     mapPoint,
   }: {
     locationName: string;
     mapPoint: UserLocation;
   }) => {
-    locationType === "pickup" 
-    ? dispatch(setPickUpLocation(mapPoint))
-    : locationType === "drop off"
-    ? dispatch(setDropOffLocation(mapPoint))
-    : ''
+    locationType === "pickup"
+      ? dispatch(setPickUpLocation(mapPoint))
+      : locationType === "drop off"
+      ? dispatch(setDropOffLocation(mapPoint))
+      : "";
     dispatch(setSearchCard(false));
   };
 
-  return {mapDataHandler, inputRef, isLoading, searchData,  searchContainer, selectedSearchItemHandler}
-}
+  return {
+    mapDataHandler,
+    inputRef,
+    isLoading,
+    searchData,
+    searchContainer,
+    selectedSearchItemHandler,
+  };
+};
