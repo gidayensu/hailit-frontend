597e80983c14378fb322767b35a421d5512a7e6e
giadyensukwabi <thurgid@gmail.com>
2024-07-05 23:08:19 +0000

    Fixed Type Error caused by userRole prop

--- components/Dashboard/Users/AllUsers.tsx
+++ components/Dashboard/Users/AllUsers.tsx
@@ -15,7 +15,7 @@ export default function AllUsers () {
             <AllUsersTable />
             }
             {selectedUserId &&
-            <UserDetails userRole = "customer"  />}
+            <UserDetails   />}
             </>
         )
 }
