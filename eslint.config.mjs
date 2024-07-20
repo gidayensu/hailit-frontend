39835245e0cb524e4621878bc9d8dc7259122afb
giadyensukwabi <thurgid@gmail.com>
2024-07-20 23:06:56 +0000

    Added Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { fixupConfigRules } from "@eslint/compat";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  // Add the following override to disable `react/prop-types` for TypeScript files
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+];
