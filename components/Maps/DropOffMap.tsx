110a8a5a5e750ddfad9c948a316ae3a6e7a0ab48
giadyensukwabi <thurgid@gmail.com>
2024-07-22 02:04:50 +0000

    Map accessible only after making delivery choices

--- components/Maps/DropOffMap.tsx
+++ components/Maps/DropOffMap.tsx
@@ -1,6 +1,16 @@
+import { useAppSelector } from "@/lib/store/hooks";
+import { redirect } from "next/navigation";
 import MainMap from "./MainMap";
 
 export default function DropOffMap() {
+  const { trip_medium, trip_area, trip_type } = useAppSelector(
+    (state) => state.deliveryChoices
+  );
+
+  if (!trip_medium || !trip_area || !trip_type) {
+    return redirect("/order");
+  }
+
   return (
     <MainMap locationType="drop off"/>  );
 }
--- components/Maps/PickUpMap.tsx
+++ components/Maps/PickUpMap.tsx
@@ -1,6 +1,17 @@
+import { useAppSelector } from "@/lib/store/hooks";
 import MainMap from "./MainMap";
+import { redirect } from "next/navigation";
 
 export default function PickUpMap() { 
+
+  const { trip_medium, trip_area, trip_type } = useAppSelector(
+    (state) => state.deliveryChoices
+  );
+
+  if (!trip_medium || !trip_area || !trip_type) {
+    return redirect("/order");
+  }
+
   return (
     <MainMap locationType="pickup"/>  );  ;
 }
