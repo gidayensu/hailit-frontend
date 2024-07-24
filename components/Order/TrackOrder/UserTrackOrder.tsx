e4be372a66ac3fa7732a76725cd66684820e744a
giadyensukwabi <thurgid@gmail.com>
2024-07-24 20:55:41 +0000

    Call/whatsapp tracking link added

--- components/Order/TrackOrder/UserTrackOrder.tsx
+++ components/Order/TrackOrder/UserTrackOrder.tsx
@@ -5,6 +5,7 @@ import Container from "@/components/ui/container";
 import { FaWhatsapp } from "react-icons/fa";
 import { MdOutlineLocalPhone } from "react-icons/md";
 import { useUserTrackOrder } from "./hooks/useUserTrackOrder";
+import Link from "next/link";
 
 export default function TrackOrder() {
   const { handleTrackTrip, inputRef } = useUserTrackOrder();
@@ -21,13 +22,20 @@ export default function TrackOrder() {
         <p className="text-center">
           Could not find package or cannot reach assigned courier?
         </p>
-        <Button variant={"outline"} className="w-2/3 ">
-          <MdOutlineLocalPhone className="text-lg mr-2" /> Call Management
-        </Button>
+        <div className="w-full flex items-center justify-center gap-2 ">
 
-        <Button className="w-2/3 bg-green-500 text-white">
-          <FaWhatsapp className="text-lg mr-2" /> WhatsApp
+        <Link href={'tel:+233291546476'} className="max-w-w-1/3 flex items-center justify-center">
+        <Button variant={"outline"} className="hover:bg-primary-color hover:text-white">
+          <MdOutlineLocalPhone className="text-lg " /> Call
         </Button>
+        </Link>
+
+      <Link href={'https://wa.me/+233291546476'} className="max-w-2/3 flex items-center justify-center ">
+        <Button className=" hover:bg-green-500 hover:text-white text-green-500 bg-white border border-green-500">
+          <FaWhatsapp className="text-lg " /> WhatsApp
+        </Button>
+      </Link>
+        </div>
       </Container>
     </>
   );
