88846f28abeea7ffc718863fdb85086ce1a1b4be
giadyensukwabi <thurgid@gmail.com>
2024-07-28 00:54:40 +0000

    Fixed type error

--- components/Dashboard/TrackOrder/TrackOrderFormDash.tsx
+++ components/Dashboard/TrackOrder/TrackOrderFormDash.tsx
@@ -1,15 +1,13 @@
 "use client";
 
 import TrackOrderForm from "@/components/Form/TrackOrderForm";
-//hook
-import { useGetTrip } from "./StatusSection/hooks/useGetTrip";
 
 export default function TrackOrderFormDash() {
-  const { inputRef, handleTrackTrip } = useGetTrip();
+  
 
   return (
     <main className="flex flex-col gap-5  md:h-full md:mb-0  ">
-      <TrackOrderForm inputRef={inputRef} onClickFunc={handleTrackTrip} />
+      <TrackOrderForm />
     </main>
   );
 }
