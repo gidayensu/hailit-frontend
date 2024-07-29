6a9c93d8549339a69228b64177e7e575cf8927b9
giadyensukwabi <thurgid@gmail.com>
2024-07-29 03:29:11 +0000

    'Okay' if a notice otherwise, 'close'

--- components/Shared/SecondaryModal.tsx
+++ components/Shared/SecondaryModal.tsx
@@ -67,7 +67,7 @@ export default function SecondaryModal({
                 onClick={closeModal}
               >
                 
-                Close
+                {note ? "Okay" : "Close"}
               </Button>
             </div>
           </div>
